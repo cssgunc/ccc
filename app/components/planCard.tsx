98714406f1d3d3615d4c2c2d590450b382a30316
Maxwell Hu <maxhu@ad.unc.edu>
2025-11-19 19:52:58 -0500

    figma research page

--- app/components/planCard.tsx
+++ app/components/planCard.tsx
@@ -8,30 +8,25 @@ interface PlanCardProps {
 }
 
 const BLUE_COLOR = "text-[#499ED7]";
-const BORDER_COLOR = "border-[#499ED7]";
 
 export default function PlanCard({
     title = "Placeholder Plan Title",
     summary = "Placeholder description for the plan card component.",
 }: PlanCardProps) {
     return (
-        <div
-            className={
-                "mx-auto px-16 py-10 rounded-3xl bg-white border-4 " +
-                BORDER_COLOR
-            }
-        >
-            <div className="flex flex-col items-center">
-                <h2 className={"text-4xl font-bold pb-3.5 " + BLUE_COLOR}>
-                    {title}
-                </h2>
-                <p
-                    className={
-                        "text-2xl font-medium leading-9 max-w-5xl " + BLUE_COLOR
-                    }
-                >
-                    {summary}
-                </p>
+        <div className="mx-auto max-w-6xl rounded-4xl pt-10 pb-10 px-10">
+            <div>
+                <div>
+                    <h2
+                        className={`text-5xl font-bold mb-4 ${BLUE_COLOR} flex`}
+                    >
+                        {title}
+                    </h2>
+
+                    <p className="text-[28px] font-medium leading-11">
+                        {summary}
+                    </p>
+                </div>
             </div>
         </div>
     );
