21e0365fd4b12d3245c0dc66aeeef97858e74991
anikaahmed114 <aahm@unc.edu>
2026-01-27 20:30:11 -0500

    Fixed chart deployment bug

--- app/routes/data.tsx
+++ app/routes/data.tsx
@@ -159,10 +159,7 @@ export function Chart({
                     <CardDescription>{activeConfig.subtitle}</CardDescription>
                 </div>
                 <Select value={timeRange} onValueChange={setTimeRange}>
-                    <SelectTrigger
-                        className="hidden w-[40] rounded-lg sm:ml-auto sm:flex border-gray-400"
-                        aria-label="Select a value"
-                    >
+                    <SelectTrigger className="hidden w-[40] rounded-lg sm:ml-auto sm:flex border-gray-400">
                         <SelectValue
                             placeholder="All years"
                             className="border-gray-400"
