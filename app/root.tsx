150550a2320833d13f315c934440a63e49b42daf
Toren Kristoffersen <kristtor@unc.edu>
2026-02-03 15:55:58 -0500

    Added vercel analytics to root

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
     ScrollRestoration,
 } from "react-router";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -48,6 +50,7 @@ export default function App() {
     return (
         <>
             <Outlet />
+            <Analytics />
         </>
     );
 }
